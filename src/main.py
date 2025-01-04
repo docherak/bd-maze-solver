d43b959cb0957aa8c1a81fa1f8372be4ade8c907
docherak <docherak@protonmail.ch>
2025-01-04 16:43:54 +0100

    feat: init Point and Line classes
    Implement drawing lines.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
-from window import Window
+from window import Window, Point, Line
 
 
 def main():
     win = Window(800, 600)
+
+    a = Point(x=40, y=100)
+    b = Point(x=600, y=300)
+
+    line = Line(a, b)
+
+    win.draw_line(line, "blue")
+
     win.wait_for_close()
 
 
--- src/window.py
+++ src/window.py
@@ -21,3 +21,28 @@ class Window:
 
     def close(self):
         self.__running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point():
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+
+class Line():
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+                self.a.x,
+                self.a.y,
+                self.b.x,
+                self.b.y,
+                fill=fill_color,
+                width=2
+        )
